a7a9c09562db1ffe2a1f26428913b74a70aa4a72
1014205876 <1014205876@qq.com>
2018-11-19 18:03:27 +0800

    ok

--- 线上标准文件夹/server/server.js
+++ 线上标准文件夹/server/server.js
@@ -1,93 +1,72 @@
 const express = require('express');
 const path = require('path');
-const jwt = require('jsonwebtoken'); // 使用jwt签名
+const ConnectCas = require('connect-cas2');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const MemoryStore = require('session-memory-store')(session);
 const proxy = require('http-proxy-middleware');
 const app = express();
-const http = require('http');  
-const qs = require('querystring'); 
+const upload = require('./upload');
 const server = app.listen(process.env.NODE_PORT);
 
-app.use(bodyParser.json({limit:'50mb'}));
+app.use(cookieParser());
+app.use(session({
+    name: 'NSESSIONID',
+    secret: 'Hello I am a long long long secret',
+    resave: true,//添加这行
+    saveUninitialized: true,//添加这行
+    store: new MemoryStore()  // or other session store
+}));
 
-app.use(bodyParser.urlencoded({ extended: true,limit:'50mb'}));
+const casClient = new ConnectCas({
+    ignore: [
+        /\/ignore/
+    ],
+    match: [],
+    servicePrefix: process.env.NODE_SERVER,
+    serverPath: process.env.CAS_SERVER,
+    paths: {
+        validate: '/cas/validate',
+        serviceValidate: '/cas/serviceValidate',
+        // proxy: '/cas/proxy',
+        login: '/cas/login',
+        logout: '/cas/logout',
+        proxyCallback: ''
+    },
+    redirect: false,
+    gateway: false,
+    renew: false,
+    slo: true,
+    cache: {
+        enable: false,
+        ttl: 5 * 60 * 1000,
+        filter: []
+    },
+    fromAjax: {
+        header: 'x-client-ajax',
+        status: 418
+    }
+});
 
-app.post('/api/auth/login', function (appReq, res) {
-    //用户名+密码 发送给后台
-    
-    userInfos = {"loginName":appReq.body.loginName,"password":appReq.body.password};
-    // console.log(JSON.stringify(userInfos));
-    var content = JSON.stringify(userInfos);
-    var options = {
-        hostname: process.env.ZUUL_HOST,  //服务器id
-        port: process.env.ZUUL_PORT,    //服务器端口号
-        path: '/peak-resource/v1/resource/users',
-        method: 'POST',  
-        headers: {  
-            'Content-Type': 'application/json;charset=utf-8'  
-        }  
-    };
-      
-    var req;
-    req = http.request(options, function (e) {
-        e.setEncoding('utf8');
-        e.on('data', function (user) {
-            //根据后端返回值，判断用户名和密码
-            user=JSON.parse(user);
-            console.log("user",user)
-            if(user.code == "200"){
-                // 将用户名通过jwt加密token
-                var token = jwt.sign({ name: appReq.body.loginName }, 'secret', {
-                    expiresIn: 60 * 60 * 24// 授权时效24小时
-                });
-                res.json({
-                    "code": "200",
-                    "reason": "",
-                    "result": {
-                        "token": token
-                    }
-                });
-            }
-            else {
-                res.json(user)
-            }  
-        });
-    });  
-      
-    req.on('error', function (e) {
-        console.log('problem with request: ' + e.message);  
-    });  
-      
-    // write data to request body  
-    req.write(content);  
-      
-    req.end();  
-    
-})
+app.use(casClient.core());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//静态页面的入口文件夹
+app.use(express.static(path.join(__dirname, 'dist')));
+//文件上传接口
+app.use('/file-upload', upload);
+//登出cas
+app.get('/logout', casClient.logout());
 //拦截所有/api请求添加头信息后转发
 app.all('/api\*', function (req, res) {
-    let appId = process.env.AUTH_ID;
-    let token = req.headers.authorization;
-    if(token){
-        // 解码 token (验证 secret 和检查有效期（exp）)
-        jwt.verify(token,'secret',function(error,decoded){
-           if(error){
-               return res.json({"code": "401","reason":"无效的token","result":""})
-           }else{
-            req.decoded=decoded;
-           }
-        })
-    }else{
-        return res.json({"code": "401","reason":"没有找到token","result":""})
-    }
     let superagent = require('superagent');
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=UTF-8' });
+    //let charset = require("superagent-charset");
+    //charset(superagent);
     let path = req.path.replace('/api', '');
-    let url = process.env.ZUUL_URL + path;
+    let url = process.env.API_SERVER + path;
+    res.writeHead(200, {'Content-Type': 'application/json;charset=UTF-8'});
     var sreq;
     if (req.method == 'GET') {
         sreq = superagent.get(url);
@@ -111,15 +90,17 @@ app.all('/api\*', function (req, res) {
             .send(req.body);
     }
     sreq
-        .set('AUTH_ID', appId)
-        .set('AUTH_USER', req.decoded.name)
+        //.charset('utf8')
+        .set('AUTH_ID', process.env.AUTH_ID)
+        .set('AUTH_USER',encodeURI(req.session.cas.user))
         .pipe(res)
         .on('end', function () {
             console.log(`请求${path}已完成！`);
         });
 });
-//静态页面的入口文件夹
-app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
 //把路由交给angular管理
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
